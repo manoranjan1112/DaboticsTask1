440687c74c4c048d0b7a82700ae6883ddfbbdfc5
Manoranjan kumar singh <manoranjan4152@gmail.com>
2023-08-02 22:06:29 +0530

    DABOTICTASK1

---
+++ javascript/chapter1.js
@@ -0,0 +1,23 @@
+let a="234";
+let b="mantu";
+console.log(a+b);
+console.log(typeof a+b);
+
+const a1={
+    name: "harry",
+    section: 1,
+    isprinciple: false,
+}
+//a1=45
+//a1={}
+a1['friend']="shubham"
+a1['name']="Lovish"
+console.log(a1)
+
+const dic={
+    Athleisure: "Comfortable and casual footwear & clothing designed for exercise and rigorous activity.",
+    Broigus:"Angry or irritated.",
+    ByCatch: "A catch of fish that cannot be put to commercial use.",
+    Blert:	"A cowardly person, someone who is weak.", 
+}
+console.log(dic.Blert);
